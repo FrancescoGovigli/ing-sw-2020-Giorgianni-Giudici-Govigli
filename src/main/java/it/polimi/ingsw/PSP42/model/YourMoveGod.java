54fc8d073ee1047b5a94955bf480bbd2baa9cd54
Francesco <francesco.govigli@mail.polimi.it>
2020-04-03 23:17:02 +0200

    Apollo Card added & Playerstate saved

--- src/main/java/it/polimi/ingsw/PSP42/model/YourMoveGod.java
+++ src/main/java/it/polimi/ingsw/PSP42/model/YourMoveGod.java
@@ -1,8 +1,21 @@
 package it.polimi.ingsw.PSP42.model;
 
-public class YourMoveGod extends SimpleGod{
+public abstract class YourMoveGod extends SimpleGod {
 
-    public YourMoveGod(Worker w) {
-        super(w);
+    public YourMoveGod(Worker w1,Worker w2){
+        super(w1,w2);
+    }
+
+    public abstract boolean powerMoveAvailable(int x,int y,Worker w);
+    public abstract void setPowerPosition(int x, int y,Worker w);
+
+    @Override
+    public boolean powerAvailable(int x, int y,Worker w) {
+        return this.powerMoveAvailable(x,y,w);
+    }
+
+    @Override
+    public void setPower(int x, int y,Worker w) {
+        this.setPowerPosition(x,y,w);
     }
 }
